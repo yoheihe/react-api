ad10f0efe9cd3b57adbdd28cd9ddd75130ba137d
yohei Yuda <yuyu.history.journey@gmail.com>
2024-06-13 19:01:52 +0900

    テーブル作成

--- api/src/App.js
+++ api/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  // 全ての投稿を取得
+  const fetchPosts = () => {
+    console.log('step1');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json().then(data => {
+        console.log('step2');
+        console.log(data);
+        setPosts(data);
+      }))
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+
+    // test 
+    console.log('step3');
+  };
+  const id = "ID"
+  const title = "Title"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Results</h2>
+      <button onClick={fetchPosts}>Fetch Posts</button>
+      
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+ 
+    
     </div>
   );
-}
+};
 
 export default App;
